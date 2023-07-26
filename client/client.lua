d1d183e3093878033dfd292604f21cdcd6c9a16a
goodnight <56773210+g00dnlght@users.noreply.github.com>
2023-07-26 14:35:23 -0700

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,4 +1,4 @@
-local spawnCallback
+--[[ local spawnCallback
 local firstSpawn = true
 local spawnPos = vector3(-36.27580642700195, 171.19190979003907, 94.99284362792969)
 
@@ -10,25 +10,11 @@ AddEventHandler('onClientResourceStart', function(resourceName)
   spawnCallback = function()
     if firstSpawn then
       -- Spawn them at the spawnpoint
-      exports.spawnmanager:spawnPlayer({
-        x = spawnPos.x,
-        y = spawnPos.y,
-        z = spawnPos.z,
-        heading = 301.30,
-        model = 'ig_roccopelosi',
-        skipFade = false
-      })
+      exports.spawnmanager:spawnPlayer({x = spawnPos.x, y = spawnPos.y, z = spawnPos.z, heading = 301.30, model = 'ig_roccopelosi', skipFade = false})
       firstSpawn = false
     else
       -- Respawn them at the same location
-      exports.spawnmanager:spawnPlayer({
-        x = spawnPos.x,
-        y = spawnPos.y,
-        z = spawnPos.z,
-        heading = 301.30,
-        skipFade = false
-        -- 'model' is left out here so that their ped model is preserved
-      }, function(spawn)
+      exports.spawnmanager:spawnPlayer({x = spawnPos.x, y = spawnPos.y, z = spawnPos.z, heading = 301.30, skipFade = false}, function(spawn)
         ClearPedBloodDamage(PlayerPedId())
       end)
     end
@@ -37,3 +23,4 @@ AddEventHandler('onClientResourceStart', function(resourceName)
   exports.spawnmanager:setAutoSpawnCallback(spawnCallback)
   exports.spawnmanager:setAutoSpawn(true)
 end)
+ ]] 
